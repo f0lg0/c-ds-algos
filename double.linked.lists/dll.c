32e3a2ed29ddca2a4849b7c94f65aca4ed2c992f
f0lg0 <folgonileonardo@gmail.com>
2020-12-03 22:11:49 +0100

    new: double linked list

---
+++ double.linked.lists/dll.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * node: a piece of data containing a numeric value, a pointer to the right node and a pointer to the left one
+*/
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+typedef struct node Node;
+Node *start = NULL;
+
+
+/**
+ * createNode: creates a new Node
+ * @param val the value to place into the Node's data
+ * @return pointer to new Node
+*/
+Node* createNode(int val) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+/**
+ * initList: initialize a dll of size "size" with nodes containing a 0
+ * @param size the size of the list
+ * @return void 
+*/
+void initList(int size) {
+    Node *newNode;
+    Node *tempNode;
+
+    for (int i = 0; i < size; i++) {
+        newNode = createNode(0);
+        if (!start) {
+            start = newNode;
+        } else {
+            tempNode = start;
+            while (tempNode->right) {
+                tempNode = tempNode->right;
+            }
+            tempNode->right = newNode;
+            newNode->left = tempNode;
+        }
+    }
+}
+
+/**
+ * countNodes: counts the number of nodes in a list
+ * @param start the start of the list
+ * @return number of nodes
+*/
+int countNodes(Node *start) {
+    if (!start)
+        return 0;
+
+    return 1 + countNodes(start->right);
+}
+
+/**
+ * insertAtBeginning: insert a node at the beginning of the list
+ * @param value the value of the node 
+ * @return void
+*/
+void insertAtBeginning(int val) {
+    Node *newNode = createNode(val);
+
+    if (!start) {
+        start = newNode;
+    } else {
+        newNode->right = start;
+        start->left = newNode;
+        start = newNode;
+    }
+}
+
+/**
+ * insertAtEnd: insert a node at the end of the list
+ * @param value the value of the node
+ * @return void
+*/
+void insertAtEnd(int val) {
+    Node *newNode = createNode(val);
+
+    if (!start) {
+        start = newNode;
+    } else {
+        Node *tempNode = start;
+        while (tempNode->right) {
+            tempNode = tempNode->right;
+        }
+        tempNode->right = newNode;
+        newNode->left = tempNode;
+    }
+}
+
+/**
+ * insertAtMid: insert a node at a given position
+ * @param value the node's value
+ * @param pos the position you want the node to be in
+ * @return void
+*/
+void insertAtMid(int val, int pos) {
+    int nodesCount = countNodes(start);
+
+    if (pos > 1 && pos < nodesCount) {
+        Node *newNode = createNode(val);
+        Node *tempNode, *prevNode;
+        tempNode = prevNode = start;
+
+        int i = 1;
+        while (i < pos) {
+            prevNode = tempNode;
+            tempNode = tempNode->right;
+            i++;
+        }
+        
+        newNode->left = tempNode;
+        newNode->right = tempNode->right;
+        tempNode->right->left = newNode;
+        tempNode->right = newNode;
+
+    } else {
+        printf("[ERR] Invalid postion.\n");
+    }
+
+}
+
+/**
+ * deleteAtBeginning: deletes the first node of the list
+ * @return void
+*/
+void deleteAtBeginning() {
+    if (!start) {
+        printf("[WARNING] Empty list\n");
+    } else {
+        Node *tempNode = start;
+        start = start->right;
+        start->left = NULL;
+
+        free(tempNode);
+    }
+}
+
+/**
+ * deleteAtEnd: deletes the last node of the list
+ * @return void
+*/
+void deleteAtEnd() {
+    if (!start) {
+        printf("[WARNING] Empty list\n");
+    } else {
+        Node *tempNode = start;
+        while (tempNode->right) {
+            tempNode = tempNode->right;
+        }
+        tempNode->left->right = NULL;
+        free(tempNode);
+    }
+}
+
+/**
+ * deleteAtMid: deletes a node at a given position
+ * @param pos the node's  position
+ * @return void
+*/
+void deleteAtMid(int pos) {
+    if (!start) {
+        printf("[WARNING] Empty list\n");
+    } else {
+        int nodesCount = countNodes(start);
+
+        if (pos > 1 && pos < nodesCount) {
+            Node *tempNode = start;
+
+            int i = 1;
+            while (i < pos) {
+                tempNode = tempNode->right;
+                i++;
+            }
+            tempNode->right->left = tempNode->left;
+            tempNode->left->right = tempNode->right;
+            free(tempNode);
+        } else {
+            printf("[ERR] Invalid position.\n");
+        }
+    }
+}
+
+/**
+ * deleteAll: delete entire list
+ * @return void
+*/
+void deleteAll() {
+    if (!start) {
+        printf("\n[ERR] List was already empty.\n");
+        return;
+    } else {
+        Node *tempNode;
+
+        while (start) {
+            tempNode = start;
+            start = start->right;
+
+            free(tempNode);
+        }
+    }
+}
+
+/**
+ * traverse: traverse the list left to right with recursion
+ * @param start the start node
+ * @return void
+*/
+void traverse(Node *start) {
+    if (!start) {
+        return;
+    } else {
+        printf("%d<->", start->data);
+        traverse(start->right);
+    }
+}
+
+/**
+ * traverse: traverse the list right to left with recursion
+ * @param start the start node
+ * @return void
+*/
+void reverseTraverse(Node *start) {
+    if (!start) {
+        return;
+    } else {
+        reverseTraverse(start->right);
+        printf("%d<->", start->data);
+    }
+}
+
+int main() {
+    return 0;
+}
---
+++ double.linked.lists/explaination.md
@@ -0,0 +1,16 @@
+# Double Linked Lists
+
+A double linked list is a two-way list in which all nodes will have two links:
+
+-   left: points to the predecessor node
+-   right: point to the successor node
+-   data
+
+![dll](../assets/dll.png)
+
+### Basic operations
+
+-   Creation
+-   Insertion
+-   Deletion
+-   Traversing
