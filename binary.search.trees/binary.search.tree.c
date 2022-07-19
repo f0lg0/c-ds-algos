2327c95e78e915a888b894e41d15389891f25598
Leonardo Folgoni <folgonileonardo@gmail.com>
2022-07-19 12:07:39 +0200

    new: deletion process for nodes with one child

--- binary.search.trees/binary.search.tree.c
+++ binary.search.trees/binary.search.tree.c
@@ -101,27 +101,51 @@ void delete(struct node** tree, uint32_t x) {
      *  - check children amount
      *  - procede with specified deletion method
      * */
-
+    
     if ((*tree)->item == x) {
         // found the node
         printf("found note at %p with value %d\n", *tree, (*tree)->item);
-
+        
+        // tmp pointer so we can call free on the deleted node
+        struct node* tmp = *tree;
+        
+        // case 1: no children
         if ((*tree)->left == NULL && (*tree)->right == NULL) {
-            printf("node has no children, deleting...\n"); 
-
-            // has no children, procede checking where the node is (right or left of parent)
+            // procede checking where the node is (right or left of parent)
+            // since we have already found the node, checking if the child item matches x is redundant
+            
             if ((*tree)->parent->left != NULL) {
-                // since we have already found the node, checking if the child item matches x is redundant
-                printf("node was on the left of %p\n", (*tree)->parent);
                 (*tree)->parent->left = NULL;
             } else {
-                printf("node was on the right of %p\n", (*tree)->parent);
                 (*tree)->parent->right = NULL;
             }
+        } else if ((*tree)->left != NULL && (*tree)->right != NULL) {
+            // case 3: has 2 children
+            printf("node has 2 children: right at %p and left at %p\n", (*tree)->right, (*tree)->left);
             
-            free(*tree);
-            printf("deleted\n");
+        } else {
+            // case 2: has one child
+            
+            if ((*tree)->left != NULL) {
+                printf("node has a child on the left at %p\n", (*tree)->left);
+
+                // deciding where to link the grandchild node
+                if ((*tree)->parent->left == *tree) {
+                    printf("the to be deleted node was on the left\n");
+                    (*tree)->parent->left = (*tree)->left;
+                } else {
+                    printf("the to be deleted node was on the left\n");
+                    (*tree)->parent->right = (*tree)->left;
+                }
+                printf("now node parent has left %p and rigth %p\n", (*tree)->parent->left, (*tree)->parent->right);
+            } else {
+                printf("node has a child on the right at %p\n", (*tree)->right);
+            }
         }
+
+        printf("freeing node at %p\n", (tmp));
+        free(tmp);
+
         return;
     }
 
@@ -133,17 +157,18 @@ void delete(struct node** tree, uint32_t x) {
 
 int32_t main() {
     struct node* root = create_node(10);
-    printf("root: %p\n", root);
+    printf("root at %p with value %d\n", root, root->item);
 
     insert(&root, 12, root);
     insert(&root, 6, root);
     insert(&root, 3, root);
     insert(&root, 1, root);
     insert(&root, 99, root);
+    // insert(&root, 5, root);
 
     traverse(root);
 
-    delete(&root, 99);
+    delete(&root, 3);
 
     traverse(root);
 
