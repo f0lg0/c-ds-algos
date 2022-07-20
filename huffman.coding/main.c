555999e21c7cc9d59b58afdbfe74500ead2ab8b1
Leonardo Folgoni <folgonileonardo@gmail.com>
2022-07-20 13:42:25 +0200

    new: unfinished huffman coding

---
+++ huffman.coding/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/**
+ *  input: BCAADDDCCACACAC
+ *  input_size = 120 bits
+ *
+ *  B_frequency: 1
+ *  C_frequency: 6
+ *  A_frequency: 5
+ *  D_frequency: D
+ *
+ *  q = [B, D, A, C] increasing order of frequency
+ * */
+
+int32_t main() {
+    printf("hello\n");
+    return 0;
+}
